c5bf56b6c672944683fee63e8c8d652806e2e7ad
wojciechhicu <trustme@onet.pl>
2022-07-17 21:50:56 +0200

    added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
+import { MatBadgeModule } from '@angular/material/badge';
 
 // components
 import { AppRoutingModule } from './app-routing.module';
@@ -15,12 +16,28 @@ import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StatsComponent } from './pixelscan/stats/stats.component';
+import { BlocksComponent } from './pixelscan/blocks/blocks.component';
+import { ValidatorsComponent } from './pixelscan/validators/validators.component';
+import { WalletsComponent } from './me/wallets/wallets.component';
+import { TransactionsComponent } from './me/transactions/transactions.component';
+import { Error404Component } from './errors/error404/error404.component';
+import { AboutComponent } from './other/about/about.component';
+import { DocumentationComponent } from './other/documentation/documentation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
-    ToolbarComponent
+    ToolbarComponent,
+    StatsComponent,
+    BlocksComponent,
+    ValidatorsComponent,
+    WalletsComponent,
+    TransactionsComponent,
+    Error404Component,
+    AboutComponent,
+    DocumentationComponent
   ],
   imports: [
     BrowserModule,
@@ -30,7 +47,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
-    MatListModule
+    MatListModule,
+    MatBadgeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
