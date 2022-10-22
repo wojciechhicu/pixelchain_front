a69395dec95806c274f2c2c5d8b58687b78560c3
Wojciech Marek <trustme@onet.pl>
2022-10-22 23:45:32 +0200

    added icons

--- src/app/me/transactions/create-tx/create-tx.component.ts
+++ src/app/me/transactions/create-tx/create-tx.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-create-tx',
-  templateUrl: './create-tx.component.html',
-  styleUrls: ['./create-tx.component.scss']
+	selector: 'app-create-tx',
+	templateUrl: './create-tx.component.html',
+	styleUrls: ['./create-tx.component.scss'],
 })
 export class CreateTxComponent implements OnInit {
+	
+	from: string = '';
+	to: string = '';
+	txValue!: number;
+	fee!: number;
 
-  constructor() { }
+	constructor() {}
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {}
 
+	sendTransaction(from: string, to: string, txValue: number, fee: number){
+		
+	}
 }
